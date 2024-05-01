d96de0bbb560f4295ece63aecad2179e0d5fe829
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-04-30 20:40:01 -0700

    Make adjustable curve

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -7,6 +7,9 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab;
 
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +17,15 @@ public class Graph : MonoBehaviour
 
     void Awake()
     {
-        for (int i = -100; i < 100; i++)
+        float step = 2f / resolution;
+        for (int i = 0; i < resolution; i++)
         {
-            int x = i;
-            int y = (x * x * x) / 400;
+            float x = ((float)i + 0.5f) * step - 1f;
+            float y = (x * x);
             Transform point = Instantiate(pointPrefab);
+            point.localScale = Vector3.one * step;
             point.localPosition = new Vector3(x, y, 0);
+            point.SetParent(transform, false);
         }
     }
 
